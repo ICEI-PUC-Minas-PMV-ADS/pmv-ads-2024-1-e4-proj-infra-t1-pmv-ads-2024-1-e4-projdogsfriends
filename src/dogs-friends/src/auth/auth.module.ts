da4923dd541b6f98c2285a4fd91efa67c9eb686b
jeosafa eller <jeosafaeller@gmail.com>
2024-03-31 17:29:09 -0300

    JWT token feat

--- src/dogs-friends/src/auth/auth.module.ts
+++ src/dogs-friends/src/auth/auth.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { JwtStrategy } from "./strategy/jwt.strategy";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
 
-    
+    imports:[JwtModule.register({})],
     controllers: [AuthController],
-    providers: [AuthService]
+    providers: [AuthService, JwtStrategy]
     
 })
 
--- src/dogs-friends/src/auth/auth.service.ts
+++ src/dogs-friends/src/auth/auth.service.ts
@@ -5,25 +5,26 @@ import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import * as argon from 'argon2';
 import { ConfigService } from "@nestjs/config";
 import { LoginDto } from "./dto/login.dto";
+import { JwtService } from "@nestjs/jwt";
 @Injectable()
-export class AuthService{
-    constructor(private prisma: PrismaService, private config:ConfigService){}
-    
-        
-       
-    
-    async create(dto:AuthDto){
+export class AuthService {
+    constructor(private prisma: PrismaService, private config: ConfigService,  private jwt:JwtService) { }
 
-        
+
+
+
+    async create(dto: AuthDto) {
+
+
         const senha = await argon.hash(dto.senha)
 
-        try{
-            
-            
+        try {
+
+
 
             const cliente = await this.prisma.cliente.create({
 
-                data:{
+                data: {
                     email: dto.email,
                     senha,
                     nome: dto.nome,
@@ -31,9 +32,9 @@ export class AuthService{
                     cpf: dto.cpf,
                     fotoPerfil: dto.fotoPerfil,
                     isPasseador: dto.isPasseador,
-                    enderecos:{
-                        create:{
-                            
+                    enderecos: {
+                        create: {
+
                             logradouro: dto.enderecos.logradouro,
                             numero: dto.enderecos.numero,
                             bairro: dto.enderecos.bairro,
@@ -42,56 +43,84 @@ export class AuthService{
                             cep: dto.enderecos.cep
                         }
                     }
-                    
 
-                    
+
+
                 },
-                select:{
-                    
-                    email:true,
+                select: {
+
+                    email: true,
                 }
-                
+
             })
 
-         
-            
+
+
 
             return cliente
 
-        }catch(error){
-            if(error instanceof PrismaClientKnownRequestError){
-                if(error.code === 'P2002'){
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
                     throw new ForbiddenException('Email já cadastrado')
                 }
-            }throw error
+            } throw error
         }
-        
+
     }
 
-    async signin(dto: LoginDto){
-        
+    async signin(dto: LoginDto) {
+
         //find user by email
         const user =
-        await this.prisma.cliente.findUnique({
-            where:{
-                email:dto.email,
-            },
-           
-        });
-        
-         //if user does not exist, throw an error
-         if(!user)
-         throw new ForbiddenException('Credentials not valid');
-         //compare password
-         const pwMatches = await argon.verify(user.senha, dto.senha);
-            if(!pwMatches)
+            await this.prisma.cliente.findUnique({
+                where: {
+                    email: dto.email,
+                },
+
+            });
+
+        //if user does not exist, throw an error
+        if (!user)
             throw new ForbiddenException('Credentials not valid');
-            //return a message
-            
-       
+        //compare password
+        const pwMatches = await argon.verify(user.senha, dto.senha);
+        if (!pwMatches)
+            throw new ForbiddenException('Credentials not valid');
+        //return a message
+
+
 
         delete user.senha
-        return user   
-       
+        return this.signToken(user.id, user.email);
+
     }
-}
+
+    async signToken(
+        clienteId: string,
+        email: string,
+    ): Promise<{ access_token: string }> {
+        const payload = {
+            sub: clienteId,
+            email,
+        };
+
+        const secret = this.config.get('JWT_SECRET');
+
+        const token = await this.jwt.signAsync(
+            payload, {
+            expiresIn: '15m',
+            secret: secret,
+        })
+
+
+
+        return {
+
+            access_token: token
+        }
+
+    }
+
+
+    }
