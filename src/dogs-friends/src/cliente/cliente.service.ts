b0d022b5d7bf71766ccfb12e6566d33e44739940
pauloosilas <paulo3356@gmail.com>
2024-04-03 19:37:05 -0300

    refactor search

--- src/dogs-friends/src/cliente/cliente.service.ts
+++ src/dogs-friends/src/cliente/cliente.service.ts
@@ -70,6 +70,9 @@ export class ClienteService {
             where:{
               AND: [
                 {
+                    isPasseador: true
+                },
+                {
                     enderecos:{
                         some:{
                             cidade:{
