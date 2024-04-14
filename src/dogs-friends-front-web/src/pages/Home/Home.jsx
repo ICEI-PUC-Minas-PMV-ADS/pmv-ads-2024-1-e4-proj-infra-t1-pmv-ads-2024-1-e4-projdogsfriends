b409c4319a7685afbf825c6e51a83261e0776d33
Felipe Carneiro <correaflp@gmail.com>
2024-04-14 00:27:16 -0300

    Remake Home component and CSS styles

--- src/dogs-friends-front-web/src/pages/Home/Home.jsx
+++ src/dogs-friends-front-web/src/pages/Home/Home.jsx
@@ -1,52 +1,57 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
-import { Link } from 'react-router-dom';
-
 
 export const Home = () => {
-
-
   return (
-
     <div className="home-container">
-
       <header className="home-header">
-        <h1 className="home-title">
-          Bem-vindo ao PetFriend, nossa plataforma para passeios de pets!
-        </h1>
-        
-        <Link to="/auth/login" className="home-button-connect">Conectar-se</Link>
-        {/* <button className="home-button-connect" href="auth/login" >Conectar-se</button> */}
+        <img src="" alt="Logo" className="home-logo" />
+
+        <nav className="home-nav">
+          {/*<Link to="/" className="nav-link">
+            Início
+  </Link>*/}
+          <Link to="/about" className="nav-link">
+            Sobre
+          </Link>
+          {/*<Link to="/services" className="nav-link">
+            Serviços
+          </Link>*/}
+          {/*<Link to="/contact" className="nav-link">
+            Contato
+          </Link>*/}
+        </nav>
+
+        <Link to="/auth/login" className="home-button-connect">
+          Conectar-se
+        </Link>
       </header>
 
-      
-      <img
-        className="home-image"
-        src="https://www.treehugger.com/thmb/Xb43cjwU8jrZHf7ZVFwkvYTGH7I=/5939x3965/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2019__06__Dog_Walk_Loose_Leash-267825dd05914e57b03ca569d6cb11ba.jpg"
-        // src="img/img1.jpeg"
-        alt="Pet walking"
-      />
-      <p className="home-description">
-        Aqui, tutores podem encontrar profissionais qualificados para
-        passear com seus pets.
-      </p>
-      <Link to="agenda-passeador" className="home-button">Agendar um passeio</Link>
-      {/* <button className="home-button">Encontre um passeador</button> */}
-      
+      <div className="home-content">
+        <p className="home-message">
+          Bem-vindo ao PetFriend, nossa plataforma para passeios de pets!
+        </p>
+
+        <img
+          className="home-image"
+          src="https://www.treehugger.com/thmb/Xb43cjwU8jrZHf7ZVFwkvYTGH7I=/5939x3965/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2019__06__Dog_Walk_Loose_Leash-267825dd05914e57b03ca569d6cb11ba.jpg"
+          alt="Pet walking"
+        />
+      </div>
 
       <footer className="home-footer">
-        <a href="https://www.facebook.com" className="social-link">
+        {/*<a href="https://facebook.com" className="social-link">
           Facebook
         </a>
-        <a href="https://www.x.com" className="social-link">
+        <a href="https://twitter.com" className="social-link">
           Twitter
         </a>
-        <a href="https://www.instagram.com" className="social-link">
+        <a href="https://instagram.com" className="social-link">
           Instagram
-        </a>
+        </a>*/}
+        <p>2024 - PetFriend</p>
       </footer>
-
-
     </div>
   );
 };
