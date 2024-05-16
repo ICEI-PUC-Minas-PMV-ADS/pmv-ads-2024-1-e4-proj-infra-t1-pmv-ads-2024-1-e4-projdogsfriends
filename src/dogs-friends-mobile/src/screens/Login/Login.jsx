4e26438943c10ecd59fdac407a185fbac1474f53
Jeosafa_Eller <jeosafaeller@gmail.com>
2024-05-16 08:44:48 -0300

    tela de login estilizada

--- src/dogs-friends-mobile/src/screens/Login/Login.jsx
+++ src/dogs-friends-mobile/src/screens/Login/Login.jsx
@@ -1,12 +1,149 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, {useState}from 'react'
+import { Alert, Button, Image, Pressable, SafeAreaView, StyleSheet, Switch, Text, TextInput, View } from 'react-native'
 
 const Login = () => {
+  const [click,setClick] = useState(false);
+     const {username,setUsername}=  useState("");
+    const {password,setPassword}=  useState("");
+const logo = require('../../../assets/images/logo.png');
   return (
-   <View>
-    <Text>Login</Text>
+  
+   <SafeAreaView style={styles.container}>
+        
+   <Image source={logo} style={styles.image} resizeMode='contain' />
+   <Text style={styles.title}>Login</Text>
+   <View style={styles.inputView}>
+       <TextInput style={styles.input} placeholder='EMAIL' value={username} onChangeText={setUsername} autoCorrect={false}
+   autoCapitalize='none' />
+       <TextInput style={styles.input} placeholder='SENHA' secureTextEntry value={password} onChangeText={setPassword} autoCorrect={false}
+   autoCapitalize='none'/>
    </View>
-  )
+   <View style={styles.rememberView}>
+       <View style={styles.switch}>
+           <Switch  value={click} onValueChange={setClick} trackColor={{true : "green" , false : "gray"}} />
+           <Text style={styles.rememberText}>Lembrar-me</Text>
+       </View>
+       <View>
+           <Pressable onPress={() => Alert.alert("Forget Password!")}>
+               <Text style={styles.forgetText}>Esqueceu a senha?</Text>
+           </Pressable>
+       </View>
+   </View>
+
+   <View style={styles.buttonView}>
+       <Pressable style={styles.button} onPress={() => Alert.alert("Login Successfuly!","see you in my instagram if you have questions : must_ait6")}>
+           <Text style={styles.buttonText}>LOGIN</Text>
+       </Pressable>
+      
+   </View>
+   
+
+
+   <Text style={styles.footerText}>Não tem cadastro?<Text style={styles.signup}>  Cadastre-se</Text></Text>
+
+   
+</SafeAreaView>
+)
 }
+
+
+const styles = StyleSheet.create({
+container : {
+alignItems : "center",
+paddingTop: 70,
+},
+image : {
+height : 160,
+width : 170
+},
+title : {
+fontSize : 30,
+fontWeight : "bold",
+textTransform : "uppercase",
+textAlign: "center",
+paddingVertical : 40,
+color : "blue"
+},
+inputView : {
+gap : 15,
+width : "100%",
+paddingHorizontal : 40,
+marginBottom  :5
+},
+input : {
+height : 50,
+paddingHorizontal : 20,
+borderColor : "blue",
+borderWidth : 1,
+borderRadius: 7
+},
+rememberView : {
+width : "100%",
+paddingHorizontal : 50,
+justifyContent: "space-between",
+alignItems : "center",
+flexDirection : "row",
+marginBottom : 8
+},
+switch :{
+flexDirection : "row",
+gap : 1,
+justifyContent : "center",
+alignItems : "center"
+
+},
+rememberText : {
+fontSize: 13
+},
+forgetText : {
+fontSize : 11,
+color : "blue"
+},
+button : {
+backgroundColor : "blue",
+height : 45,
+borderColor : "gray",
+borderWidth  : 1,
+borderRadius : 5,
+alignItems : "center",
+justifyContent : "center"
+},
+buttonText : {
+color : "white"  ,
+fontSize: 18,
+fontWeight : "bold"
+}, 
+buttonView :{
+width :"100%",
+paddingHorizontal : 50
+},
+optionsText : {
+textAlign : "center",
+paddingVertical : 10,
+color : "gray",
+fontSize : 13,
+marginBottom : 6
+},
+mediaIcons : {
+flexDirection : "row",
+gap : 15,
+alignItems: "center",
+justifyContent : "center",
+marginBottom : 23
+},
+icons : {
+width : 40,
+height: 40,
+},
+footerText : {
+textAlign: "center",
+color : "gray",
+},
+signup : {
+color : "blue",
+},
+  
+
+})
 
 export default Login
